48b6fa251641ebc84281314bb9b042eaaf981e85
Anastas <odhiseaanastas@gmail.com>
2022-10-01 13:06:00 +0100

    some changes

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-const menu = document.querySelector(".menu");
-const items = document.querySelectorAll(".item");
-const hamburger= document.querySelector(".hamburger");
-const close= document.querySelector(".close");
-const menuicon = document.querySelector(".menuicon");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
-function toggleMenu() {
-  if (menu.classList.contains("showMenu")) {
-    menu.classList.remove("showMenu");
-    close.style.display = "none";
-    menuicon.style.display = "block";
-  } else {
-    menu.classList.add("showMenu");
-    close.style.display = "block";
-    menuicon.style.display = "none";
-  }
-}
+hamburger.addEventListener("click",()=>{
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
 
-hamburger.addEventListener("click", toggleMenu);
+document.querySelectorAll(".nav-link").forEach(n => n
+    .addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    }))
